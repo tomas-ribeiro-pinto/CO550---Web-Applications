9f021c9625c1614b6b458ae609944745731fb551
tomas-ribeiro-pinto <morato.toms@outlook.com>
2022-10-06 17:34:42 +0100

    Simple Currency Converter

---
+++ CurrencyConverter/CurrencyConverter/Pages/Index.cshtml.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CurrencyConverter.Pages;
+
+public class IndexModel : PageModel
+{
+    private readonly ILogger<IndexModel> _logger;
+
+    [BindProperty]
+    public string Amount { get; set; }
+    private const double RATE = 1.14;
+
+    public IndexModel(ILogger<IndexModel> logger)
+    {
+        _logger = logger;
+    }
+
+    public void OnGet()
+    {
+
+    }
+
+    public void OnPost()
+    {
+        double result;
+        result = Convert.ToDouble(Amount) * RATE;
+        ViewData["Message"] = $"{Amount} GBP is {result.ToString("0.00")} EUR";
+    }
+}
+
